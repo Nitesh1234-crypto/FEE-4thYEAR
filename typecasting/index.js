e68c77ba5a227c35f01463eb825f9080b82ed821
Nitesh <nitesh.e21@nsut.ac.in>
2024-07-08 22:33:44 +0530

    typecasting:

---
+++ typecasting/index.js
@@ -0,0 +1,18 @@
+console.log(typeof (1 + "1"))
+console.log(1-"1");
+
+// console.log(Number("abc"))
+console.log(1-"abc")
+console.log(1 + +"6")
+console.log(Number(""))
+console.log(0==[])
+console.log(String([]))
+console.log("hi");
+console.log(""==[]) //true
+console.log(0=="") //true
+console.log("" == " ")
+console.log({}=={}) //false
+
+let arr=[1,2,3];
+let arr2=arr;
+console.log(arr==arr2)
